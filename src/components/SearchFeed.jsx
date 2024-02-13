bbb80b068daa692a2990f84cff1eb2ab84d79b0e
Yash Shekhar <yashshekhar2934@gmail.com>
2024-02-13 23:59:00 +0530

    first commit

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from './';
+
+const SearchFeed = function() {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => { fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+  .then((data) => setVideos(data.items))
+   }, [searchTerm]);
+
+  return (
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        <span style={{ color: '#c43f86'}}>{searchTerm}</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    
+  )
+}
+
+export default SearchFeed
